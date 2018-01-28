3a5c2c347b9d3ccc9e56b741b6eb507d07eb3128
Attila PAPP <darkplant@gmail.com>
2018-01-28 14:36:13 +0000

    Title Screen (scene) + script

---
+++ TransMission/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+
+    public Text newGameText;
+    public Text exitGameText;
+    public Button startText;
+    public Button exitText;
+    // Use this for initialization
+    void Start () {
+        startText = startText.GetComponent<Button>();
+        exitText = exitText.GetComponent<Button>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    public void StartGame()
+    {
+        // SceneManager.LoadScene("Difficulty");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
